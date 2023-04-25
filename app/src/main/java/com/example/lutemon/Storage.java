25d9412e63d4d44bf807f2d74df9b372b56f09ea
SamiAnttalainen <sami_anttalainen@hotmail.com>
2023-04-25 21:28:46 +0300

    Some training and battlefield functionalities.
    (cherry picked from commit 907f815a84c59acf2dc722a1203bb9a2e64cfd69)

--- app/src/main/java/com/example/lutemon/Storage.java
+++ app/src/main/java/com/example/lutemon/Storage.java
@@ -2,101 +2,57 @@ package com.example.lutemon;
 
 import android.content.Context;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Storage {
-    protected static Storage storage = null;
+
+
     protected String name;
-    private ArrayList<Lutemon> lutemonsAtHome = new ArrayList<>();
-    private ArrayList<Lutemon> lutemonsAtTraining = new ArrayList<>();
-    private ArrayList<Lutemon> lutemonsAtBattle = new ArrayList<>();
-    private ArrayList<Lutemon> lutemonsAtDead = new ArrayList<>();
-    private ArrayList<ArrayList<Lutemon>> lutemonListsForSaving = new ArrayList<>();
-
-    Storage(String name) {}
-    private Storage() {}
-    public static Storage GetInstance() {
-        if (storage == null) {
-            storage = new Storage();
-        }
-        return storage;
+    protected ArrayList<Lutemon> lutemons = new ArrayList<>();
+    public Storage(String name) {
+        this.name = name;
     }
 
-    public String getName() {
-        return name;
+    public void addLutemon(Lutemon lutemon) {
+        lutemons.add(lutemon);
     }
 
-    public ArrayList<Lutemon> getLutemonsAtHome() {
-        return lutemonsAtHome;
-    }
-
-    public ArrayList<Lutemon> getLutemonsAtTraining() {
-        return lutemonsAtTraining;
-    }
-
-    public ArrayList<Lutemon> getLutemonsAtBattle() {
-        return lutemonsAtBattle;
-    }
-
-    public ArrayList<Lutemon> getLutemonsAtDead() {
-        return lutemonsAtDead;
+    public Lutemon getLutemon(int id) {
+        return lutemons.get(id);
     }
 
     public ArrayList<Lutemon> getLutemons() {
-        ArrayList<Lutemon> lutemons = new ArrayList<>();
-        lutemons.addAll(lutemonsAtHome);
-        lutemons.addAll(lutemonsAtTraining);
-        lutemons.addAll(lutemonsAtBattle);
-        lutemons.addAll(lutemonsAtDead);
         return lutemons;
     }
-    public ArrayList<ArrayList<Lutemon>> getLutemonListsForSaving() {
-        lutemonListsForSaving.clear();
-        lutemonListsForSaving.add(lutemonsAtHome);
-        lutemonListsForSaving.add(lutemonsAtTraining);
-        lutemonListsForSaving.add(lutemonsAtBattle);
-        lutemonListsForSaving.add(lutemonsAtDead);
-        return lutemonListsForSaving;
-    }
-    public void addLutemon(Lutemon lutemon) {
-        lutemonsAtHome.add(lutemon);
-    }
 
-    public void moveLutemon(ArrayList<Lutemon> fromList, Lutemon lutemon, ArrayList<Lutemon> toList) {
-        fromList.remove(lutemon);
-        toList.add(lutemon);
-    }
+    public void listLutemons() {
+        for (Lutemon lutemon : lutemons) {
 
+        }
+    }
 
     public void saveLutemons(Context context) {
         try {
-            ObjectOutputStream saveLutemons = new ObjectOutputStream(context.openFileOutput("lutemons.data", Context.MODE_PRIVATE));
-            saveLutemons.writeObject(getLutemonListsForSaving());
-            System.out.println("Lutemonit tallennettu.");
-            saveLutemons.close();
+            ObjectOutputStream saveLutemon = new ObjectOutputStream(context.openFileOutput("lutemons.data", Context.MODE_PRIVATE));
+            saveLutemon.writeObject(lutemons);
+            saveLutemon.close();
         } catch (IOException e) {
-            System.out.println("Lutemonien tallentaminen epäonnistui.");
+            System.out.println("Lutemonin tallentaminen epäonnistui.");
         }
     }
 
     public void loadLutemons(Context context) {
         try {
             ObjectInputStream loadLutemons = new ObjectInputStream(context.openFileInput("lutemons.data"));
-            Storage.GetInstance().lutemonListsForSaving = (ArrayList<ArrayList<Lutemon>>) loadLutemons.readObject();
+            lutemons = (ArrayList<Lutemon>) loadLutemons.readObject();
             loadLutemons.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Datatiedostoa ei löytynyt.");
-            e.printStackTrace();
         } catch (IOException e) {
             System.out.println("Lutemonien lataaminen epäonnistui.");
-            e.printStackTrace();
         } catch (ClassNotFoundException e) {
             System.out.println("Lutemonien lataaminen epäonnistui.");
-            e.printStackTrace();
         }
     }
 
