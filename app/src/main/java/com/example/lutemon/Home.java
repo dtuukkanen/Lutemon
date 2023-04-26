5896fc3670c657f74a1a2b9dec16fc5b08f17658
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-04-26 10:22:31 +0300

    Updated home

--- app/src/main/java/com/example/lutemon/Home.java
+++ app/src/main/java/com/example/lutemon/Home.java
@@ -4,18 +4,6 @@ import java.util.ArrayList;
 
 public class Home extends Storage{
 
-    public void addLutemon(Lutemon lutemon) {
-        lutemons.add(lutemon);
-    }
-
-    public Lutemon getLutemon(int id) {
-        return lutemons.get(id);
-    }
-
-    public ArrayList<Lutemon> listLutemons() {
-        return lutemons;
-    }
-
     public static void createLutemon(Lutemon lutemon) {
         lutemons.add(lutemon);
     }
