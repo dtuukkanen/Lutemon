8815451a15984891fffa78bd93ecfca34bd79107
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-04-27 14:03:31 +0300

    Fighting developed further

--- app/src/main/java/com/example/lutemon/BattleActivity.java
+++ app/src/main/java/com/example/lutemon/BattleActivity.java
@@ -7,6 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.CheckBox;
 
+import com.example.lutemon.Lutemons.Lutemon;
+
+import java.util.ArrayList;
+
 public class BattleActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
@@ -20,6 +24,9 @@ public class BattleActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.rvFighterList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new CheckboxAdapter(getApplicationContext(), Storage.getInstance().getBattlefield().getLutemons()));
+    }
 
+    public void figth(ArrayList<Lutemon> lutemons) {
+        Storage.getInstance().getBattlefield().fight(lutemons.get(0), lutemons.get(1));
     }
 }
